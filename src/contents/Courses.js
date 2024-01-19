8838ab8605ff03ed5332bc4638b592a3d6b0c20d
vinithp03 <vinithpoojary2001@gmail.com>
2024-01-19 21:28:47 +0530

    updated commit

--- src/contents/Courses.js
+++ src/contents/Courses.js
@@ -1,29 +1,35 @@
 import React from 'react';
 import './Courses.css'; // Import your CSS file for styling
+import Button from '@mui/material/Button';
+import SendIcon from '@mui/icons-material/Send';
 
 const Courses = () => {
   return (
     <div className='crs'>
-      <h2 className="titl"  style={{ color: '#FFA500',  marginLeft:'30px' }}>Courses/Certificates</h2>
+      <h2 className="titl"  style={{ color: '#FFA500',  marginLeft:'30px' , paddingTop:'20px' }}>Courses/Certificates</h2>
     <div className="courses">
       <div className="course-item">
         <h3>Java Full Stack Course</h3>
         <p>Institute: J-Spiders, Bangalore</p>
         <p>Duration: 4 months</p>
-        {/* Add a link to the Java Full Stack certificate when available */}
-        <p>Certificate: [Link to Certificate]</p>
       </div>
 
       <div className="course-item">
         <h3>Web Development Course</h3>
         <p>Institute: J-Spiders, Bangalore</p>
         <p>Duration: 2 months</p>
-        {/* Add a link to the Web Development certificate when available */}
-        <p>Certificate: [Link to Certificate]</p>
       </div>
 
-      {/* Add more course items as needed */}
+      <div className="course-item">
+        <h3>SQL Course</h3>
+        <p>Institute: J-Spiders, Bangalore</p>
+        <p>Duration: 2 months</p> 
+      </div>
+
     </div>
+    <p className='certificate'>Certificate : <Button className='cirti' variant="contained" endIcon={<SendIcon />}>
+    <a className='anchor' href="../Vinith_course_cirtificate.png">Check it out</a>
+      </Button></p>
     </div>
   );
 };
