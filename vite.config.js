2a5088e12b4d4ca0d4bede473f0dde83704066df
vinithp03 <vinithpoojary2001@gmail.com>
2024-08-21 22:02:00 +0530

    Updates

--- vite.config.js
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  base: "/Vinith_PersonalPortfolio_website_React/", // Update this line
-});
