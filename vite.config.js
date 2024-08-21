77b7e972b46c42960b731f3a9ef5c50b104256ba
vinithp03 <vinithpoojary2001@gmail.com>
2024-08-21 21:19:59 +0530

    Add dist directory and update vite.config.js for GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  base: "/Vinith_PersonalPortfolio_website_React/", // Update this line
+});
